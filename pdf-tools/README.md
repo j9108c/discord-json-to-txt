3c9b33f9aa885f8bcc737cb00b6130b18f1d484c
j9108c <j9108c@outlook.com>
2021-07-13 05:14:32 -0400

    .

--- pdf-tools/README.md
+++ pdf-tools/README.md
@@ -1,3 +1,5 @@
 # pdf-tools
-- flag "-c" = combine
-- flag "-o" = ocr
+- use only one flag at a time::
+	- flag "-c" = combine
+	- flag "-o" = ocr
+	- flag "-p" = ppt/pptx to pdf. windows only (i.e., run in ps/cmd). [source](https://stackoverflow.com/a/31624001/7417159)
--- pdf-tools/script.py
+++ pdf-tools/script.py
@@ -1,38 +1,49 @@
 import os
 import sys
-import pdfrw
-import ocrmypdf
 
 project_root = os.getcwd()
-out_path = f"{project_root}/# output.pdf"
 
-combined = False
+if "-c" in sys.argv[1:]: # combine flag
+	import pdfrw
 
-if ("-c" in sys.argv[1:]): # combine flag
 	writer = pdfrw.PdfWriter()
 
 	for file in os.listdir(project_root):
-		if (file.endswith(".pdf")):
+		if file.endswith(".pdf"):
+			print(file)
 			pdf_path = f"{project_root}/{file}"
 			writer.addpages(pdfrw.PdfReader(pdf_path).pages)
 
 	writer.trailer.Info = pdfrw.IndirectPdfDict(Title="")
-	writer.write(out_path)
-	combined = True
+	writer.write(f"{project_root}/# output.pdf")
+elif "-o" in sys.argv[1:]: # ocr flag
+	import ocrmypdf
 
-if ("-o" in sys.argv[1:]): # ocr flag
-	in_path = None
-	if (combined):
-		in_path = out_path
-	else:
-		for file in os.listdir(project_root):
-			if (file.endswith(".pdf")):
-				in_path = f"{project_root}/{file}"
-				break
+	for file in os.listdir(project_root):
+		if file.endswith(".pdf"):
+			print(file)
+			pdf_path = f"{project_root}/{file}"
 
-	pid = os.fork()
-	if (pid > 0): # parent process
-		os.waitpid(pid, 0) # wait for child process to end
-	elif (pid == 0): # child process
-		ocrmypdf.ocr(input_file=in_path, output_file=out_path, force_ocr=True, output_type="pdf", language="eng")
-		os._exit(0)
+			pid = os.fork()
+			if pid > 0: # parent process
+				os.waitpid(pid, 0) # wait for child process to end
+			elif pid == 0: # child process
+				ocrmypdf.ocr(input_file=pdf_path, output_file=f"{os.path.splitext(ppt_path)[0]} (ocr).pdf", force_ocr=True, output_type="pdf", language="eng")
+				os._exit(0)
+elif "-p" in sys.argv[1:]: # ppt flag
+	import comtypes.client
+
+	powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+	powerpoint.Visible = 1
+
+	for file in os.listdir(project_root):
+		if file.endswith(".ppt") or file.endswith(".pptx"):
+			print(file)
+			ppt_path = f"{project_root}/{file}"
+			deck = powerpoint.Presentations.Open(ppt_path)
+			deck.SaveAs(f"{os.path.splitext(ppt_path)[0]}.pdf", 32) # https://docs.microsoft.com/en-us/office/vba/api/powerpoint.ppsaveasfiletype "ppSaveAsPDF"
+			deck.Close()
+			
+	powerpoint.Quit()
+else:
+	print("invalid args")
